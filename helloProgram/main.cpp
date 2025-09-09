e680ff5caf757365e6fe22e69a39e1664f5f13e8
lindsaycullum <lindsay.cullum@ocdsb.ca>
2025-09-09 19:42:36 +0000

    Demo cin cout

---
+++ helloProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number;
+    cout << "What is your favourite number?";
+    cin >> number;
+    cout << "Your favourite number is " << number << endl;
+}
